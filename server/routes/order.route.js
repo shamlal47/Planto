648aaf9e857ca1540b76ebbe1e60b1f554cc2ecc
Mohammed shamlal kp <shamlalthegreat@gmail.com>
2025-06-22 13:29:36 +0530

    order changes

--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -1,15 +1,13 @@
 import { Router } from "express";
+import { createOrder, deleteOrder, getOrder, getOrders } from "../controllers/order.controller.js";
+import { authorize } from "../middleware/auth.middleware.js";
 
 const orderRouter = Router();
-orderRouter.post('/create', (req, res) => {
-    res.status(201).json({ message: "Order created successfully" });
-});
-orderRouter.get('/list', (req, res) => {
-    res.status(200).json({ message: "List of orders" });
-});
-orderRouter.get('/details/:id', (req, res) => {
-    const orderId = req.params.id;
-    res.status(200).json({ message: `Details of order ${orderId}` });
-});
+
+orderRouter.post('/create', authorize ,createOrder);
+orderRouter.get('/list', getOrders);
+orderRouter.get('/details/:id', getOrder);
+orderRouter.delete('/delete/:id', deleteOrder);
+
 
 export default orderRouter;
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
 
 const userRouter = Router();
-userRouter.get('/', (req, res) => {
-    res.status(200).json({ message: "User profile fetched successfully" });
-});
-userRouter.get('/:id', (req, res) => {
-    const userId = req.params.id;
-    res.status(200).json({ message: `User profile of ${userId} fetched successfully` });
-});
+
+userRouter.get('/', getUsers);
+
+userRouter.get('/:id', getUser);
+
 userRouter.put('/:id', (req, res) => {
     const userId = req.params.id;
     res.status(200).json({ message: `User profile of ${userId} updated successfully` });
